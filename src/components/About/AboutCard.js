a6335360cccbac3a262c0bce17bd55bb95d7d2b6
Erikabermeo97 <124104722+Erikabermeo97@users.noreply.github.com>
2023-03-23 20:30:44 -0400

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,20 +8,19 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, I am <span className="purple">Erika Bermeo</span>
+            from <span className="purple"> Cuenca, Ecuador.</span>
+            <br />I am a student of Web App Design at Paier College.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Apart to be a student, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Hiking
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -29,7 +28,7 @@ function AboutCard() {
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Life is a journey, enjoy the ride !"{" "}
           </p>
           <footer className="blockquote-footer">Soumyajit</footer>
         </blockquote>
