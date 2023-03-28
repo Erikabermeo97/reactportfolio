2aa165c249624cc2361d879ffe1827ba11ac1813
Erikabermeo97 <124104722+Erikabermeo97@users.noreply.github.com>
2023-03-28 19:09:44 -0400

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-"https://raw.githubusercontent.com/erikabermeo97/portfolio/master/src/Assets/erika.pdf";
+"https://github.com/Erikabermeo97/reactportfolio/blob/572fc6e37565aed000e1c7c7e78b580d896a1df3/src/Assets/erika.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
